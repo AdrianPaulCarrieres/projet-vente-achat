630ca201ec4e8e4b785a3da6bbb229d8107ed464
loicbtd <loic.bert.marcel@gmail.com>
2019-11-29 10:43:01 -0500

    amélioration de la strucutre du projet

--- application-client/application-codrova/www/js/vue/AccueilVue.js
+++ application-client/application-codrova/www/js/vue/AccueilVue.js
@@ -1,14 +1,13 @@
 var AccueilVue = (function() {
 
-    framgmentNavigation = document.getElementById("fragment-navigation").innerHTML;
+    framgmentBarreNavigationHaut = document.getElementById("fragment-barre-navigation-haut").innerHTML;
     pageAccueil = document.getElementById("page-accueil").innerHTML;
     
     return function() {
         this.afficher = function() {
 
             elementBody = document.getElementsByTagName("body")[0];
-            console.log(framgmentNavigation);
-            elementBody.innerHTML = framgmentNavigation + pageAccueil;        
+            elementBody.innerHTML = framgmentBarreNavigationHaut + pageAccueil;        
         }
     }
 })();
