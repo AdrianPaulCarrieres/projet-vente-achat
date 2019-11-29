3e5fb49459e10de6d6e49a90bbc30998141cea9e
EstevanGAY <estevan.gay@gmail.com>
2019-11-29 11:25:23 -0500

    Ajout des modèles de données

---
+++ application-client/application-codrova/www/js/modele/Categorie.js
@@ -0,0 +1,6 @@
+module.exports = {
+    Categorie: function (idCategorie, etiquette) {
+        this.id_categorie = idCategorie;
+        this.etiquette = etiquette;
+    }
+}
---
+++ application-client/application-codrova/www/js/modele/Chaine.js
@@ -0,0 +1,11 @@
+module.exports = {
+    Chaine: function (idChaine, etiquette, categorie, prix, diametre, marque, modele) {
+        this.id_chaine = idChaine;
+        this.etiquette = etiquette;
+        this.categorie = categorie;
+        this.prix = prix;
+        this.diametre = diametre;
+        this.marque = marque;
+        this.modele = modele;
+    }
+}
