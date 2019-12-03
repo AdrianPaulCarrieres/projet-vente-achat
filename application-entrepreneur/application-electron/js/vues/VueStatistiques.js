c43d88d15eab6d4c49f758a31c2a26b05f4331e2
Louis Pascuzzi <louis.pascuzzi@edu.univ-fcomte.fr>
2019-12-03 18:10:33 -0500

    ajout de jquerry et de la fonction peuplerTableauAnnee

--- application-entrepreneur/application-electron/js/vues/VueStatistiques.js
+++ application-entrepreneur/application-electron/js/vues/VueStatistiques.js
@@ -8,7 +8,30 @@ var VueStatistiques = (function () {
             console.log("affichage");
             elementBody = document.getElementsByTagName("body")[0];
             elementBody.innerHTML = navbar + pageStatistiques;
+            peuplerTableauAnnee();
+            peuplerTab
         }
+    };
 
+    function peuplerTableauAnnee(){
+        tableauAnnee = document.getElementById("corps_tableau_anneau");
+        var nombre, total, moyenne, meilleurProduit, meilleureCategorie;
+        nombre = 200;
+        total = "$508.20";
+        moyenne = "$2.50";
+        meilleurProduit = "veste arc'teryx";
+        meilleureCategorie = "ski";
+        var textTableau = "";
+        for (let i = 0; i < 13; i++) {
+            textTableau += "<tr>"
+                +"<th>"+i+"</th>"
+                +"<th>"+nombre+"</th>"
+                +"<th>"+total+"</th>"
+                +"<th>"+moyenne+"</th>"
+                +"<th>"+meilleurProduit+"</th>"
+                +"<th>"+meilleureCategorie+"</th>"
+                +"</tr>"
+        }
+        tableauAnnee.innerHTML = textTableau;
     }
 })();
