8e6f4bddba3b51daafd0ee1fb11ad91a2cf294d5
EstevanGAY <estevan.gay@gmail.com>
2019-12-03 16:37:51 -0500

    Ajout des denriers modèles

---
+++ application-client/application-codrova/www/js/modele/Chaine.js
@@ -0,0 +1,12 @@
+module.exports = {
+    Chaine: function (idChaine, etiquette, categorie, prix, diametre, marque, modele) {
+        this.id_chaine = idChaine;
+        this.etiquette = etiquette;
+        this.categorie = categorie;
+        this.prix = prix;
+        this.diametre = diametre;
+        this.taille = taille;
+        this.marque = marque;
+        this.modele = modele;
+    }
+}
