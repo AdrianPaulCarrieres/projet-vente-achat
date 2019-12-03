cc39309ea23bf1a6d2104adffa94d2b7404e88a3
EstevanGAY <estevan.gay@gmail.com>
2019-12-03 17:31:13 -0500

    Ajout du modèle d'achat

---
+++ application-client/application-codrova/www/js/modele/Achat.js
@@ -0,0 +1,11 @@
+module.exports = {
+    Achat: function (idAchat, idProduit, idPersonnalisation, dateAchat, prix, nomClient, mailClient) {
+        this.id_achat = idAchat;
+        this.id_produit = idProduit;
+        this.id_personnalisation = idPersonnalisation;
+        this.date_achat = dateAchat;
+        this.prix = prix;
+        this.nom_client = nomClient;
+        this.mail_client = mailClient;
+    }
+}
