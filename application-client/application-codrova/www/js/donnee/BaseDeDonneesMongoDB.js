2210634397d0ca363c945bac28500178fd57369d
EstevanGAY <estevan.gay@gmail.com>
2019-12-03 17:19:47 -0500

    AJout de la modification/suppression de 1 ou plusieurs docuements

--- application-client/application-codrova/www/js/donnee/BaseDeDonneesMongoDB.js
+++ application-client/application-codrova/www/js/donnee/BaseDeDonneesMongoDB.js
@@ -1,14 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
 
-// TODO: A mettre a jour
 const credentials = require('../../../../../../credentials/Credentials');
 
-const credentialsMongo = credentials.Credentials();
+const credentialsMongo = new credentials.Credentials();
 
 exports.url = function () {
     return credentialsMongo.url;
 }
-
 
 exports.dbName = function () {
     return credentialsMongo.dbName;
@@ -33,7 +31,7 @@ exports.insererTableauElement = async function (tableauValeur, collection) {
 
 }
 
-exports.insererElement = async function (element, collection) {
+exports.insererDocument = async function (element, collection) {
 
     var client = this.client();
 
@@ -72,5 +70,66 @@ exports.selectionnerDocumentsCollection = async function (collection) {
     var resultat = await db.collection(collection).find({}).toArray();
 
     return resultat;
+
+}
+
+exports.modifierUnDocument = async function (clef, valeur, nouveauDocument, collection) {
+
+    var client = this.client();
+    const document = JSON.parse('{"' + clef + '":' + valeur + '}');
+
+    const c = await client.connect();
+
+    const db = c.db(this.dbName());
+
+    await db.collection(collection).updateOne(document, { $set: nouveauDocument });
+
+    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Document modifié');
+
+}
+
+exports.modifierDesDocuments = async function (clef, valeur, nouveauDocument, collection) {
+
+    var client = this.client();
+    const document = JSON.parse('{"' + clef + '":' + valeur + '}');
+
+    const c = await client.connect();
+
+    const db = c.db(this.dbName());
+
+    await db.collection(collection).updateMany(document, { $set: nouveauDocument });
+
+    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Documents modifiés');
+
+}
+
+
+exports.supprimerUnDocument = async function (clef, valeur, collection) {
+
+    var client = this.client();
+    const document = JSON.parse('{"' + clef + '":' + valeur + '}');
+
+    const c = await client.connect();
+
+    const db = c.db(this.dbName());
+
+    await db.collection(collection).deleteOne(document);
+
+    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Document supprimé');
+
+}
+
+exports.supprimerDesDocuments = async function (clef, valeur, collection) {
+
+    var client = this.client();
+    const document = JSON.parse('{"' + clef + '":' + valeur + '}');
+
+    const c = await client.connect();
+
+    const db = c.db(this.dbName());
+
+    await db.collection(collection).deleteMany(document);
+
+    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Documents supprimés');
 
 }
