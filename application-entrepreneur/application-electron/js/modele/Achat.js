61cad9c9a6a9c761a99750041001a2fd2994c592
EstevanGAY <estevan.gay@gmail.com>
2019-12-06 10:30:33 -0500

    Modification de modele achat

--- application-entrepreneur/application-electron/js/modele/Achat.js
+++ application-entrepreneur/application-electron/js/modele/Achat.js
@@ -1,11 +1,12 @@
 exports.COLLECTION = 'achat';
 
 module.exports = {
-    Achat: function (idAchat, idProduit, idPersonnalisation, dateAchat, prix, nomClient, mailClient) {
+    Achat: function (idAchat, idProduit, idPersonnalisation, dateAchat, moisAchat, prix, nomClient, mailClient) {
         this.id_achat = idAchat;
         this.id_produit = idProduit;
         this.id_personnalisation = idPersonnalisation;
         this.date_achat = dateAchat;
+        this.mois_achat = moisAchat;
         this.prix = prix;
         this.nom_client = nomClient;
         this.mail_client = mailClient;
